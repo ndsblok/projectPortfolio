02964aff7fefffb422453bd0eb70cd73f2f460c4
ndsblock <nebotovd@gmail.com>
2022-12-08 18:22:07 +0300

    123

--- script/main.js
+++ script/main.js
@@ -1,9 +1,26 @@
-$('.header-arrow').click(() => {
-    $('html, body').animate({
-        scrollTop: $('.portfolio').offset().top
-    }, 1000);
+// arrow
+
+$(".header-arrow").click(() => {
+  $("html, body").animate(
+    {
+      scrollTop: $(".portfolio").offset().top,
+    },
+    1000
+  );
 });
 
-$('.btn').click(function(){
-    $(".block_with_text").fadeToggle(100);
-  }); 
+// button
+
+$(".btn").click(function () {
+  $(".block_with_text").fadeToggle(100);
+});
+
+// header
+
+$(window).on("load", function () {
+  $("#header").vide("./video/cover"),
+    {
+      bgColor: "rgba(36, 36, 36, 0.822)",
+    };
+});
+
