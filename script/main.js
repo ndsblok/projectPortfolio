7700339d985b27d7ee215b5da20db1704ac8084e
Nebotov Dmitry <118490831+ndsblok@users.noreply.github.com>
2022-12-11 23:12:29 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -14,12 +14,3 @@ $(".header-arrow").click(() => {
 $(".btn").click(function () {
   $(".block_with_text").fadeToggle(100);
 });
-
-// header
-
-$(window).on("load", function () {
-  $("#header").vide("./video/cover"),
-    {
-      bgColor: "rgba(36, 36, 36, 0.822)",
-    };
-});
