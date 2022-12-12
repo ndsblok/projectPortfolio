edf7e5847525e8812821183d4dc8b89111ea8d76
ndsblock <nebotovd@gmail.com>
2022-12-13 02:39:51 +0300

    123

--- script/main.js
+++ script/main.js
@@ -5,12 +5,12 @@ $(".header-arrow").click(() => {
     {
       scrollTop: $(".portfolio").offset().top,
     },
-    1000
+    10
   );
 });
 
 // button
 
 $(".btn").click(function () {
-  $(".block_with_text").fadeToggle(100);
+  $(".block_with_text").fadeToggle(400);
 });
